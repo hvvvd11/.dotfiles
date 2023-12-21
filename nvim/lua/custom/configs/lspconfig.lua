c77cd80648f007d8627d83b13b9950f4440f0426
hvvvd11 <hvvvd11@gmail.com>
2023-12-21 11:07:27 +0200

    html lsp

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -26,38 +26,55 @@ lspconfig.gopls.setup {
 }
 
 -- FRONT END SHIT
+--
 lspconfig.html.setup {
-  capabilities = capabilities,
-  filetypes = {"gohtml"}
+    filetypes = { "html" },
+    on_attach = function(client, _)
+        -- Formatting on save
+        if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("LspAutocommands", { clear = true }),
+                buffer = 0,
+                callback = function()
+                    vim.lsp.buf.format({ timeout_ms = 2000 }) -- Use format instead of formatting_syn
+                end
+            })
+        end
+    end,
+    settings = {
+        html = {
+            validate = {
+                scripts = true, -- Validate JavaScript
+            }
+        }
+    }
 }
-lspconfig.tailwindcss.setup{}
+
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript" },
   cmd = { "typescript-language-server", "--stdio" }
 }
 
-lspconfig.eslint.setup({
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-  end,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  settings = {
-    codeActionOnSave = {enable = true,mode = "all"},
-    format = true,
-  }
-})
-
-lspconfig.htmx.setup{}
-
-lspconfig.emmet_ls.setup({
-  on_attach = on_attach;
-  capabilities = capabilities;
-  -- filetypes = {"gohtml"}
-})
+-- lspconfig.eslint.setup({
+--   on_attach = function(_, bufnr)
+--     vim.api.nvim_create_autocmd("BufWritePre", {
+--       buffer = bufnr,
+--       command = "EslintFixAll",
+--     })
+--   end,
+--   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+--   settings = {
+--     codeActionOnSave = {enable = true,mode = "all"},
+--     format = true,
+--   }
+-- })
+--
+-- lspconfig.emmet_ls.setup({
+--   on_attach = on_attach;
+--   capabilities = capabilities;
+--   -- filetypes = {"gohtml"}
+-- })
 
 lspconfig.cssls.setup({
   on_attach = on_attach;
